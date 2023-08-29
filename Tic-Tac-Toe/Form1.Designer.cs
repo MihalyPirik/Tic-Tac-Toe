6220a248ff47076b40b3905145394d2104b5c545
Mihaly Pirik <misipirik@gmail.com>
2023-08-29 14:35:32 +0200

    add files

---
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpTabla = new System.Windows.Forms.TableLayoutPanel();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblO = new System.Windows.Forms.Label();
+            this.btnTörlés = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.újJátékToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SzabályToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tlpTabla.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpTabla
+            // 
+            this.tlpTabla.ColumnCount = 5;
+            this.tlpTabla.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tlpTabla.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpTabla.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpTabla.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpTabla.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tlpTabla.Controls.Add(this.btn7, 1, 3);
+            this.tlpTabla.Controls.Add(this.btn8, 2, 3);
+            this.tlpTabla.Controls.Add(this.btn9, 3, 3);
+            this.tlpTabla.Controls.Add(this.btn4, 1, 2);
+            this.tlpTabla.Controls.Add(this.btn5, 2, 2);
+            this.tlpTabla.Controls.Add(this.btn6, 3, 2);
+            this.tlpTabla.Controls.Add(this.btn1, 1, 1);
+            this.tlpTabla.Controls.Add(this.btn2, 2, 1);
+            this.tlpTabla.Controls.Add(this.btn3, 3, 1);
+            this.tlpTabla.Controls.Add(this.lblX, 1, 0);
+            this.tlpTabla.Controls.Add(this.lblO, 2, 0);
+            this.tlpTabla.Controls.Add(this.btnTörlés, 3, 0);
+            this.tlpTabla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpTabla.Location = new System.Drawing.Point(0, 33);
+            this.tlpTabla.Name = "tlpTabla";
+            this.tlpTabla.RowCount = 4;
+            this.tlpTabla.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpTabla.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpTabla.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpTabla.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tlpTabla.Size = new System.Drawing.Size(982, 720);
+            this.tlpTabla.TabIndex = 0;
+            // 
+            // btn7
+            // 
+            this.btn7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn7.FlatAppearance.BorderSize = 5;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn7.Location = new System.Drawing.Point(52, 507);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(288, 210);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn8.FlatAppearance.BorderSize = 5;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn8.Location = new System.Drawing.Point(346, 507);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(288, 210);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn9.FlatAppearance.BorderSize = 5;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn9.Location = new System.Drawing.Point(640, 507);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(288, 210);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn4.FlatAppearance.BorderSize = 5;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn4.Location = new System.Drawing.Point(52, 291);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(288, 210);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn5.FlatAppearance.BorderSize = 5;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn5.Location = new System.Drawing.Point(346, 291);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(288, 210);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn6.FlatAppearance.BorderSize = 5;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn6.Location = new System.Drawing.Point(640, 291);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(288, 210);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn1.FlatAppearance.BorderSize = 5;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn1.Location = new System.Drawing.Point(52, 75);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(288, 210);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn2.FlatAppearance.BorderSize = 5;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn2.Location = new System.Drawing.Point(346, 75);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(288, 210);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn3.FlatAppearance.BorderSize = 5;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn3.Location = new System.Drawing.Point(640, 75);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(288, 210);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblX.Location = new System.Drawing.Point(52, 23);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(288, 25);
+            this.lblX.TabIndex = 9;
+            this.lblX.Text = "x győzelem: 0";
+            this.lblX.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblO
+            // 
+            this.lblO.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblO.AutoSize = true;
+            this.lblO.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblO.Location = new System.Drawing.Point(346, 23);
+            this.lblO.Name = "lblO";
+            this.lblO.Size = new System.Drawing.Size(288, 25);
+            this.lblO.TabIndex = 10;
+            this.lblO.Text = "o győzelem: 0";
+            this.lblO.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnTörlés
+            // 
+            this.btnTörlés.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTörlés.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnTörlés.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTörlés.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnTörlés.Location = new System.Drawing.Point(640, 3);
+            this.btnTörlés.Name = "btnTörlés";
+            this.btnTörlés.Size = new System.Drawing.Size(288, 66);
+            this.btnTörlés.TabIndex = 11;
+            this.btnTörlés.Text = "Törlés";
+            this.btnTörlés.UseVisualStyleBackColor = true;
+            this.btnTörlés.Click += new System.EventHandler(this.btnTörlés_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Menu;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.újJátékToolStripMenuItem,
+            this.SzabályToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(982, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // újJátékToolStripMenuItem
+            // 
+            this.újJátékToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.újJátékToolStripMenuItem.Name = "újJátékToolStripMenuItem";
+            this.újJátékToolStripMenuItem.Size = new System.Drawing.Size(84, 29);
+            this.újJátékToolStripMenuItem.Text = "Új játék";
+            this.újJátékToolStripMenuItem.Click += new System.EventHandler(this.ujJatekToolStripMenuItem_Click);
+            // 
+            // SzabályToolStripMenuItem
+            // 
+            this.SzabályToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SzabályToolStripMenuItem.Name = "SzabályToolStripMenuItem";
+            this.SzabályToolStripMenuItem.Size = new System.Drawing.Size(152, 29);
+            this.SzabályToolStripMenuItem.Text = "Szabály";
+            this.SzabályToolStripMenuItem.Click += new System.EventHandler(this.szabalyToolStripMenuItem_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 753);
+            this.Controls.Add(this.tlpTabla);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.tlpTabla.ResumeLayout(false);
+            this.tlpTabla.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpTabla;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem újJátékToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SzabályToolStripMenuItem;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblO;
+        private System.Windows.Forms.Button btnTörlés;
+    }
+}
+
---
+++ Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        char jatekos = 'x';
+        int lepesek = 0;
+        int x = 0;
+        int o = 0;
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Enabled = false;
+            if (jatekos == 'x')
+            {
+                btn.BackColor = Color.Red;
+                btn.Text = "x";
+                if (Dontes(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9))
+                {
+                    MessageBox.Show("Az x játékos nyerte a kört!");
+                    x++;
+                    lblX.Text = $"x győzelem : {x}";
+                    btn1.Enabled = false;
+                    btn2.Enabled = false;
+                    btn3.Enabled = false;
+                    btn4.Enabled = false;
+                    btn5.Enabled = false;
+                    btn6.Enabled = false;
+                    btn7.Enabled = false;
+                    btn8.Enabled = false;
+                    btn9.Enabled = false;
+                }
+                jatekos = 'o';
+            }
+            else if (jatekos == 'o')
+            {
+                btn.BackColor = Color.Green;
+                btn.Text = "o";
+                if (Dontes(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9))
+                {
+                    MessageBox.Show("Az o játékos nyerte a kört!");
+                    o++;
+                    lblO.Text = $"o győzelem : {o}";
+                    btn1.Enabled = false;
+                    btn2.Enabled = false;
+                    btn3.Enabled = false;
+                    btn4.Enabled = false;
+                    btn5.Enabled = false;
+                    btn6.Enabled = false;
+                    btn7.Enabled = false;
+                    btn8.Enabled = false;
+                    btn9.Enabled = false;
+                }
+                jatekos = 'x';
+            }
+            lepesek++;
+            if (lepesek == 9)
+            {
+                MessageBox.Show("Döntetlen!");
+                btn1.Enabled = false;
+                btn2.Enabled = false;
+                btn3.Enabled = false;
+                btn4.Enabled = false;
+                btn5.Enabled = false;
+                btn6.Enabled = false;
+                btn7.Enabled = false;
+                btn8.Enabled = false;
+                btn9.Enabled = false;
+            }
+        }
+
+        private void btnTörlés_Click(object sender, EventArgs e)
+        {
+            x = 0;
+            o = 0;
+            lblX.Text = $"x győzelem : {x}";
+            lblO.Text = $"o győzelem : {o}";
+        }
+
+        private void ujJatekToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            jatekos = 'x';
+            lepesek = 0;
+            btn1.Text = ""; btn1.BackColor = SystemColors.Control; btn1.Enabled = true;
+            btn2.Text = ""; btn2.BackColor = SystemColors.Control; btn2.Enabled = true;
+            btn3.Text = ""; btn3.BackColor = SystemColors.Control; btn3.Enabled = true;
+            btn4.Text = ""; btn4.BackColor = SystemColors.Control; btn4.Enabled = true;
+            btn5.Text = ""; btn5.BackColor = SystemColors.Control; btn5.Enabled = true;
+            btn6.Text = ""; btn6.BackColor = SystemColors.Control; btn6.Enabled = true;
+            btn7.Text = ""; btn7.BackColor = SystemColors.Control; btn7.Enabled = true;
+            btn8.Text = ""; btn8.BackColor = SystemColors.Control; btn8.Enabled = true;
+            btn9.Text = ""; btn9.BackColor = SystemColors.Control; btn9.Enabled = true;
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+        }
+
+        private void szabalyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("A két játékos X és O alakú jelekkel (bábukkal) játszik. A 3×3 mezőből álló táblára felváltva teszik le a jeleiket, bármelyik még szabad mezőre. Az nyer, akinek sikerül egy vonalban 3 jelet elhelyeznie, vízszintes, függőleges vagy átlós irányban.");
+        }
+
+        public bool Dontes(Button btn1, Button btn2, Button btn3, Button btn4, Button btn5, Button btn6, Button btn7, Button btn8, Button btn9)
+        {
+            if ((btn1.Text == btn2.Text && btn2.Text == btn3.Text && btn2.Text != "") ||
+                        (btn4.Text == btn5.Text && btn5.Text == btn6.Text && btn5.Text != "") ||
+                        (btn7.Text == btn8.Text && btn8.Text == btn9.Text && btn8.Text != "") ||
+                        (btn1.Text == btn4.Text && btn4.Text == btn7.Text && btn4.Text != "") ||
+                        (btn2.Text == btn5.Text && btn5.Text == btn8.Text && btn5.Text != "") ||
+                        (btn3.Text == btn6.Text && btn6.Text == btn9.Text && btn6.Text != "") ||
+                        (btn1.Text == btn5.Text && btn5.Text == btn9.Text && btn5.Text != "") ||
+                        (btn3.Text == btn5.Text && btn5.Text == btn7.Text && btn5.Text != ""))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
